4cef1b53bab435c40fece25afcd4363db24a093f
jugui93 <juanguillermo93@gmail.com>
2022-02-09 20:04:28 -0500

    Conceptos basicos js

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,9 @@
     const button1 = document.querySelector(".Play");
     const button2 = document.querySelector(".Mute");
 
-    const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] }) //En nuestra función especial player es una instancia del prototipo le asignaremos el valor de video para que lo reciba en configuración. Esto lo haremos con destructuración de objetos.
+    const player = new MediaPlayer({ el: video, plugins:[
+        //new AutoPlay()    
+    ] }) //En nuestra función especial player es una instancia del prototipo le asignaremos el valor de video para que lo reciba en configuración. Esto lo haremos con destructuración de objetos.
+    
     button1.onclick = () => player.togglePlay();
     button2.onclick = () => player.toggleMute();
